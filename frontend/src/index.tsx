cee2e04ea9204bdf008c5038685f20683994c72a
Graham Hargrove <ghargrove@orisintel.com>
2019-02-28 22:53:55 -0500

    Fixed babel configuration

---
+++ frontend/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App: React.SFC = () => <div>Hello from my new app</div>;
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,34 +1,34 @@
-
 var path = require('path');
 
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    devServer: {
-        historyApiFallback: true,
-        hot: true
-    },
-    entry: {
-        app: './src/index.ts'
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'babel-loader'
-            }
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({ title: 'development' }),
-        new webpack.HotModuleReplacementPlugin()
+  devServer: {
+    historyApiFallback: true,
+    hot: true
+  },
+  devtool: 'inline-source-map',
+  entry: {
+    app: './src/index.tsx'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader'
+      }
     ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
