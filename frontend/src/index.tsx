70184a4e482f7510b6bd01ce0528fe3c76823947
Graham Hargrove <ghargrove@orisintel.com>
2019-03-01 00:19:39 -0500

    Added dev dockerfile for front end

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App: React.SFC = () => <div>Hello from my new app</div>;
+const App: React.SFC = () => <div>I guess its good</div>;
 
 ReactDOM.render(<App />, document.getElementById('app'));
